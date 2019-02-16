a842375d27ea10d86fe374f29f30bdb7298f70c2
Gemmechu <gemmechumohammed@gmail.com>
2019-02-16 12:44:32 +0000

    actuator started

--- src/main/java/com/teammusika/musika/MainApplication.java
+++ src/main/java/com/teammusika/musika/MainApplication.java
@@ -1,11 +1,14 @@
 package com.teammusika.musika;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication  {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
